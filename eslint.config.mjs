3ba36b40089d86c91c2c91d2a1c6ee053cf12092
Prins Ocampo <85613393+Gambit42@users.noreply.github.com>
2025-05-09 15:10:06 +0800

    misc: update eslitn config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "off", // Disable the rule globally
     },
   },
